b615d80f041aef8c70da78a0bf341f01522b55a0
Kosalvireak Khoeun <virakvary@gmail.com>
2022-07-07 21:53:49 +0700

    commit

--- auto_comment.py
+++ auto_comment.py
@@ -1,8 +1,9 @@
+from turtle import hideturtle
 import pyautogui
 import time
 
-comments = ["model 1 size 43 @chakriya_met @twilight_anonymous @_han_nahhh_"]
-
+#comments = ["model 1 size 43 @chakriya_met @twilight_anonymous @_han_nahhh_"]
+comments =["hi"]
 # time.sleep(5)
 # pyautogui.typewrite(comments[0])
 # pyautogui.typewrite("\n")
@@ -10,9 +11,13 @@ comments = ["model 1 size 43 @chakriya_met @twilight_anonymous @_han_nahhh_"]
 
 i = 0
 
+
 while i < 100:
+
 
     pyautogui.typewrite(comments[0])
     pyautogui.typewrite("\n")
-    time.sleep(2)
+    time.sleep(0.5)
+
+
     i = i + 1
