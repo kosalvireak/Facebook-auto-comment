2d64ac50b0f7f5dde55e5e586595e3cd578953f6
Kosalvireak Khoeun <virakvary@gmail.com>
2022-07-13 14:12:41 +0700

    add bruu

--- auto_comment.py
+++ auto_comment.py
@@ -1,15 +1,17 @@
+from ctypes import BigEndianStructure
 from turtle import hideturtle
 import pyautogui
 import time
 
 #comments = ["model 1 size 43 @chakriya_met @twilight_anonymous @_han_nahhh_"]
-comments =["hi"]
+comments =["bruu"]
 # time.sleep(5)
 # pyautogui.typewrite(comments[0])
 # pyautogui.typewrite("\n")
 # time.sleep(1)
 
 i = 0
+BigEndianStructure
 
 
 while i < 100:
@@ -18,6 +20,5 @@ while i < 100:
     pyautogui.typewrite(comments[0])
     pyautogui.typewrite("\n")
     time.sleep(0.5)
-
 
     i = i + 1
