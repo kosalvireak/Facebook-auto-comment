0ca400b0262855f15db78ddb60783c749467407d
Kosalvireak Khoeun <virakvary@gmail.com>
2022-10-16 22:25:55 +0700

    add list

--- auto_comment.py
+++ auto_comment.py
@@ -1,26 +1,12 @@
-from ctypes import BigEndianStructure
-from turtle import hideturtle
 import pyautogui
 import time
 
-comments ="hello"
+comments = ["cat ", "rabbit ", "donkey", "dear", "elephant"]
 
 
+for n in comments:
+    me = "pich is " + n
 
-# time.sleep(5)
-# pyautogui.typewrite(comments[0])
-# pyautogui.typewrite("\n")
-# time.sleep(1)
-
-i = 0
-BigEndianStructure
-
-
-while i < 200:
-
-
-    pyautogui.typewrite(comments)
+    pyautogui.typewrite(me)
     pyautogui.typewrite("\n")
     time.sleep(1)
-
-    i = i + 1
